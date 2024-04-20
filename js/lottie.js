c6aaab6f06009b1b4e674abe0afb444ac5da7351
DenserMeerkat <mragulmanoharan@gmail.com>
2024-04-21 04:10:06 +0530

    feat: added hero section

---
+++ js/lottie.js
@@ -0,0 +1,15 @@
+var animation1 = bodymovin.loadAnimation({
+  container: document.getElementById("lottie-animation-1"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "./assets/hero-lottie-1.json",
+});
+
+var animation2 = bodymovin.loadAnimation({
+  container: document.getElementById("lottie-animation-2"),
+  renderer: "svg",
+  loop: true,
+  autoplay: true,
+  path: "./assets/hero-lottie-2.json",
+});
