13394a6c848498697f8ccc92793bdff3e481c0c8
DenserMeerkat <mragulmanoharan@gmail.com>
2024-04-22 03:59:29 +0530

    fix: js scripts

--- js/lottie.js
+++ js/lottie.js
@@ -1,10 +1,10 @@
-var animation1 = bodymovin.loadAnimation({
-  container: document.getElementById("lottie-animation-1"),
-  renderer: "svg",
-  loop: true,
-  autoplay: true,
-  path: "./assets/hero-lottie-1.json",
-});
+// var animation1 = bodymovin.loadAnimation({
+//   container: document.getElementById("lottie-animation-1"),
+//   renderer: "svg",
+//   loop: true,
+//   autoplay: true,
+//   path: "./assets/hero-lottie-1.json",
+// });
 
 var animation2 = bodymovin.loadAnimation({
   container: document.getElementById("lottie-animation-2"),
