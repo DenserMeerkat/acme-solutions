89976c86f149d01835d3d81d75efe3de9f649333
DenserMeerkat <mragulmanoharan@gmail.com>
2024-04-23 01:08:20 +0530

    perf: optimize and clean code

--- js/lottie.js
+++ js/lottie.js
@@ -1,15 +1,7 @@
-// var animation1 = bodymovin.loadAnimation({
-//   container: document.getElementById("lottie-animation-1"),
-//   renderer: "svg",
-//   loop: true,
-//   autoplay: true,
-//   path: "./assets/hero-lottie-1.json",
-// });
-
 var animation2 = bodymovin.loadAnimation({
-  container: document.getElementById("lottie-animation-2"),
+  container: document.getElementById("lottie-animation"),
   renderer: "svg",
   loop: true,
   autoplay: true,
-  path: "./assets/hero-lottie-2.json",
+  path: "./assets/hero-lottie.json",
 });
